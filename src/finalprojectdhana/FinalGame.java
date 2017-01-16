ee7896b93deb81c088d98c70f4b6beacbbff0321
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2017-01-16 10:00:00 -0500

    Final Game

--- src/finalprojectdhana/FinalGame.java
+++ src/finalprojectdhana/FinalGame.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.util.Random;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
@@ -26,7 +27,7 @@ public class FinalGame extends JComponent{
     long desiredTime = (1000)/desiredFPS;
     
     //game avariables
-    // colour of the background of the game
+    // creating the colour of the background
     Color background = new Color (207, 136, 185);
     
     // make the colour of the background
@@ -35,9 +36,22 @@ public class FinalGame extends JComponent{
     Rectangle [] sidebottombrick= new Rectangle [5];
     boolean [] passedbrick = new boolean [5];
     
+    // the gap between sidetopbrick and sidebottombrick
+    int brickGap = 200;
+    
+    // distance between the brick
+    int brickSpacing = 100;
+    
+    // the width of a single brick
+    int brickWidth = 150;
+    
+    // the height of a brick
+    int brickHeight = HEIGHT - 50;
+    
+    // minimum distance from edge
+    int minDistance = 200;
     
     
-
     // drawing of the game happens in here
     // we use the Graphics object, g, to perform the drawing
     // NOTE: This is already double buffered!(helps with framerate/speed)
@@ -66,7 +80,12 @@ public class FinalGame extends JComponent{
         g.fillRect(200, 50, 5, 90);
         
         // drawing the bricks that the squid needs to avoid
-        
+        g.setColor (Color.DARK_GRAY);
+        // drawing the bricks
+        for (int i = 0; i < sidetopbrick.length; i++){
+            g.fillRect(sidetopbrick[i].x,sidetopbrick [i].y,sidetopbrick [i].width, sidetopbrick[i].height);
+            g.fillRect(sidebottombrick [i].x, sidebottombrick[i].y, sidebottombrick[i].width , sidebottombrick[i]. height);
+        }
         
         
         
@@ -74,7 +93,21 @@ public class FinalGame extends JComponent{
         // GAME DRAWING ENDS HERE
     }
     
-    
+    // set up the bricks
+    public void setupthebricks (int brickPosition){
+        // a random generator
+        Random randGen = new Random ();
+        //generate the y position
+        int brickY = randGen.nextInt(HEIGHT - 2 * minDistance) + minDistance;
+        //generate the new pipe X coordinate
+        int brickX = sidetopbrick [brickPosition].x;
+        brickX = brickX + (brickWidth + brickSpacing) * sidetopbrick.length;
+        
+        sidebottombrick[brickPosition].setBounds(brickX, brickY, brickWidth, brickHeight);
+        sidetopbrick[brickPosition].setBounds(brickX, brickY - brickGap - brickHeight, brickWidth, brickHeight);
+        
+        passedbrick[brickPosition] = false;
+    }
     
     // The main game loop
     // In here is where all the logic for my game will go
@@ -85,6 +118,21 @@ public class FinalGame extends JComponent{
         long startTime;
         long deltaTime;
         
+        // set up the bricks
+        int pipeX = 600;
+        Random randGen = new Random();
+        for (int i = 0; i < sidetopbrick.length; i++) {
+            // generating a random y position
+            int pipeY = randGen.nextInt(HEIGHT - 2 * minDistance) + minDistance;
+            sidebottombrick[i] = new Rectangle(pipeX, pipeY, brickWidth, brickHeight);
+            sidetopbrick[i] = new Rectangle(pipeX, pipeY - brickGap - brickHeight, brickWidth, brickHeight);
+            
+            // move the pipeX value over
+            pipeX = pipeX + brickWidth + brickSpacing;
+            passedbrick[i] = false;
+
+        }
+         
         // the main game loop section
         // game will end if you set done = false;
         boolean done = false; 
